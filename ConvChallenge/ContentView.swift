6fde8e7654c28efca74c0234c9b597d801fe1a8c
Alex Oliveira <alexdeoliveira.poli@gmail.com>
2020-11-23 20:29:24 -0300

    Challenge complete

--- ConvChallenge/ContentView.swift
+++ ConvChallenge/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var length = ""
+	
+	let units: [UnitLength] = [.meters, .kilometers, .feet, .yards, .miles]
+	let unitsNames = ["meters", "kmeters", "feet", "yards", "miles"]
+	@State private var inputUnit = UnitLength.meters
+	@State private var outputUnit = UnitLength.meters
+	
+	var convertedLength: Double {
+		let inputLength = Double(length.replacingOccurrences(of: ",", with: ".")) ?? 0
+		let inputLengthMeasurement = Measurement(value: inputLength, unit: inputUnit)
+		
+		return inputLengthMeasurement.converted(to: outputUnit).value
+	}
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		NavigationView {
+			Form {
+				Section {
+					TextField("Length", text: $length)
+						.keyboardType(.decimalPad)
+				}
+				
+				Section(header: Text("Input Unit")) {
+					Picker("Input Unit", selection: $inputUnit) {
+						ForEach(0 ..< units.count) {
+							Text(self.unitsNames[$0]).tag(self.units[$0])
+						}
+					}
+					.pickerStyle(SegmentedPickerStyle())
+				}
+				
+				Section(header: Text("Output Unit")) {
+					Picker("Input Unit", selection: $outputUnit) {
+						ForEach(0 ..< units.count) {
+							Text(self.unitsNames[$0]).tag(self.units[$0])
+						}
+					}
+					.pickerStyle(SegmentedPickerStyle())
+				}
+				
+				Section {
+					Text("Converted length: \(convertedLength, specifier: "%.2f")")
+				}
+			}
+			.navigationBarTitle("Unit Converter")
+		}
     }
 }
 
